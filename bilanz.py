4fb6164818382015e08830e6d0b71bcc9bdb7dfe
tareklichtenfeld <110313535+tareklichtenfeld@users.noreply.github.com>
2024-12-03 07:52:37 +0100

    Add files via upload

--- bilanz.py
+++ bilanz.py
@@ -2,6 +2,11 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 
+st.set_page_config(
+    page_title="Energieimporte und Importabhängigkeit",
+    page_icon="chart_with_upwards_trend",
+    )
+    
 # Your data (cleaned up the extra spaces and colons)
 data = {
   "Jahr": ["2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022"],
